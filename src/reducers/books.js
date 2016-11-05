6ece3508c9e4ef6154d8cf2d08ddb8ee2c35dfd3
Caique Caleiro <caiquecaleiro@gmail.com>
2016-11-05 15:43:27 -0200

    Add book list container and reducer

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'A Storm of Swords' },
+    { title: 'Into the Wild' },
+    { title: 'A Game of Thrones' }
+  ];
+}
